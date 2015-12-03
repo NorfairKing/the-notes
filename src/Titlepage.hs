09feaf453ae4d4111529b796bd289905899d8a5f
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-12-03 21:28:08 +0100

    HaTeX-ified the titlepage

--- src/Titlepage.hs
+++ src/Titlepage.hs
@@ -1,11 +1,57 @@
 module Titlepage (myTitlePage) where
 
-import           Control.Monad  (void)
+import           Control.Monad (forM_)
+
+import qualified Data.Text     as T
+
 import           Notes
-import           System.Process (system)
+
+titlepageE :: LaTeXC l => l -> l
+titlepageE = liftL $ TeXEnv "titlepage" []
 
 myTitlePage :: Note
-myTitlePage = do
-  liftIO $ void $ system "git rev-parse --short HEAD > commit.tex"
-  text <- liftIO $ readFileTex "src/titlepage.tex"
-  raw text
+myTitlePage =
+    titlepageE $ do
+    raw "\n"
+    comm1 "thispagestyle" "empty"
+    comm1 "hbox" $ do
+        forM_ [1..7] $ \i -> do
+            raw "\n"
+            comm2 "rule" (raw $ T.pack $ show i ++ "pt") (commS "textheight")
+            hspace $ Pt i
+        raw "\n"
+        comm1 "hspace*" $ raw "0.05" <> commS "textwidth"
+        liftL (\a -> TeXComm "parbox" [OptArg (TeXRaw "b"), FixArg (TeXRaw "0.75\\textwidth"), FixArg a]) $ do
+            raw "\n"
+            noindent
+            huge $ textbf "The Notes"
+            lnbk
+            raw "\n"
+            raw "[2.5\\baselineskip]"
+            raw "\n"
+            large $ textsc "Tom Sydney Kerckhove"
+            lnbk
+            raw "\n"
+            raw "[4.0\\baselineskip]"
+
+            large $ tabular Nothing [LeftColumn, LeftColumn] $ do
+                "Started" & "September 28, 2015"
+                lnbk
+                "Compiled" & commS "today"
+                lnbk
+                "Commit" & input "commit.tex" -- Make this compile-time.
+                lnbk
+            comm1 "vspace" $ raw "0.5\\textheight"
+
+
+
+
+
+
+
+
+
+
+
+
+
