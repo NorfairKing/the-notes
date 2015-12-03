dcc4e7bc148d42289d73f7850e676e773702c4a6
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-12-03 15:57:01 +0100

    added hlint to pre-commit test

--- bin/pre_commit_test.sh
+++ bin/pre_commit_test.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Abort on error
 set -x
+
+# Make sure code quality is up to par
+hlint src \
+  --ignore "Redundant do" \
+  --ignore "Redundant $" \
+  --ignore "Redundant bracket"  \
+  --ignore "Parse error"  \
+  --ignore "Use concatMap"  \
+  --ignore "Reduce duplication"  \
+  --ignore "Use camelCase"  \
+  --ignore "Use import/export shortcut"  \
+  --ignore "Use ."  \
+  --ignore "Use mapM_"
+
+# Make sure the build succeeds
 make
+
+# Make sure compilation of the pdf succeeds
 ./the-notes
--- src/DataMining/MapReduce.hs
+++ src/DataMining/MapReduce.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE QuasiQuotes     #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 module DataMining.MapReduce (
     mapReduce
   ) where
