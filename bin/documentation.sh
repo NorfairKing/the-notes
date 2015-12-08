cb51f407bc7080d777c566ae2b3552c38aff4d7d
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-12-08 04:25:12 +0100

    annotated documentation consistency check

--- bin/documentation.sh
+++ bin/documentation.sh
@@ -1,1 +1,2 @@
+echo "Building documentation..."
 make doc
--- bin/pre_push_test.sh
+++ bin/pre_push_test.sh
@@ -1,8 +1,9 @@
 set -e # abort on error
 
 ./bin/pre_commit_test.sh
+./bin/documentation.sh
 
-echo "Trying out all preselections"
+echo "Trying out all preselections..."
 for i in $(ls -1 ./preselect); do
   ./preselect/$i
 done
