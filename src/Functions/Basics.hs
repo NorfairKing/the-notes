8c44f707b22de0eaec4f73cbb3beb3841f635333
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-01-08 16:02:50 +0100

    fixed the functions definition

--- src/Functions/Basics.hs
+++ src/Functions/Basics.hs
@@ -43,7 +43,6 @@ partialFunctionDefinition = de $ do
     lab functionDefinitionLabel
     lab partialFunctionDefinitionLabel
     s ["A ", partialFunction', or, function', " ", m fun_, " is a triple ", m t , " where ", m dom_, and, img_, " are sets and ", m fun_, " is a single-valued binary ", relation_, " between ", m dom_, and, m img_]
-    s ["Each of the sets ", m dom_, and, img_, " have an equivalence relation defined on them, both written as ", m (eqsign)]
     ma $ fa (cs [x, y 1, y 2]) $ (pars $ tuple x (y 1) ∈ fun_ ∧ tuple x (y 2) ∈ fun_) ⇒ (y 1 =: y 2)
     s ["The triple ", m t, " is often written as ", m $ fun fun_ dom_ img_]
     s [m dom_, " is called the ", corange', ", ", m img_, " is called the ", codomain']
