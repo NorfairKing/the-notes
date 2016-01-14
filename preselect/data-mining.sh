85de4ec418e16491cb4f3eb4a3c63b01c0096810
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-01-14 13:39:25 +0100

    Added a data mining preselect

---
+++ preselect/data-mining.sh
@@ -0,0 +1,21 @@
+./the-notes \
+  --subtitle "Data Mining" \
+  --pdf-name data-mining \
+  --tex-name data-mining \
+  --bib-name data-mining \
+  "
+  sets
+  -sets.pointed-set
+  relations
+  -relations.preorders
+  -relations.equivalence-relations
+  -relations.orders
+  functions
+  -functions.functions-and-orders
+  linear-algebra
+  geometry
+  topology
+  probability
+  statistics
+  data-mining
+  "
