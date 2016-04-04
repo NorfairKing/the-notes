ef97e85c9dfd6ab6f2e4be05be0a72a280ee340e
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-04-04 06:42:11 +0200

    The first two tiny theorems about computational problems

--- src/Cryptography/Main.hs
+++ src/Cryptography/Main.hs
@@ -14,6 +14,8 @@ import           Logic.FirstOrderLogic.Macro
 import           Probability.ProbabilityMeasure.Macro
 import           Probability.ProbabilityMeasure.Terms
 import           Relations.Orders.Macro
+import           Rings.Macro
+import           Rings.Terms
 import           Sets.Basics.Terms
 
 import           Cryptography.Macro
@@ -35,6 +37,8 @@ cryptography = chapter "Cryptography" $ do
 
     section "Computational Problems" $ do
         discreteLogarithmProblemDefinition
+        additiveDLEasy
+        dlReducable
         computationalDHProblemDefinition
         diffieHellmanTripleDefinition
         decisionalDHProblemDefinition
@@ -117,6 +121,49 @@ discreteLogarithmProblemDefinition = de $ do
         a = "a"
         g = "g"
     s [the, discreteLogarithm', "(" <> dL' <> ")", "problem", "for a", cyclic_, group, m $ grp_ =: grp (genby g) grpop_, "is the problem of computing, for a given", group, element, m $ aa ∈ grps_, "the exponent", m $ integer a, " such that", m $ aa =: g ^ a, "holds"]
+
+additiveDLEasy :: Note
+additiveDLEasy = thm $ do
+    let n = "n"
+    s [the, discreteLogarithm, "problem is trivially solvable in the", group, m $ intagrp n]
+    proof $ do
+        let z = "z"
+            a = "a"
+            g = "g"
+        s ["Recall that, for any element", m (z ∈ intmod n) <> ", we are looking for the integer", m $ integer a, "such that", m $ z =: g * a, "where", m g, "is a", generator, "of", m $ intagrp n]
+        s ["Luckily, ", m $ intagrp n, "gives rise to a", ring, m $ intring n, "as well"]
+        s ["This allows us to find", m a, "by dividing", m z, by, m g]
+        s ["More precicely: because", m g, "is a", generator, "means that", m g, "must have a multiplicative inverse in", m $ intring n, "otherwise no multiple of", m g, "would be equal to", m 1]
+        s ["Now the only thing we need to do is go through the", elements, "of", m $ intmod n, "multiply each of them by", m g, "in", m $ intring n, "and check if the result equals", m 1, "to find the multiplicative inverse", m $ rinv g, "of", m g, "in", m $ intring n]
+        s ["We then compute", m a, "by evaluating", m $ rinv g * z =: rinv g * g * a =: a]
+        s ["We could also use the extended Euclidean algorithm to find", m $ rinv g, "even more efficiently"]
+        refneeded "Extended Euclidean algorithm"
+
+dlReducable :: Note
+dlReducable = thm $ do
+    let g = "g"
+        h = "h"
+    s [the, discreteLogarithm, "problem in a", group, m grp_, "for a", generator, m g, "is reducable to the", discreteLogarithm, "problem in that same", group, "but for a different", generator]
+
+    proof $ do
+        let a = "A"
+        s ["Let", m a, "be an algorithm that solves the", discreteLogarithm, "problem for a", generator, m g]
+        s ["We construct an algorithm that solves the", discreteLogarithm, "problem for another", generator, m h, "of", m grp_, "as follows"]
+        let z = "z"
+            b = "b"
+            c = "c"
+        s ["Let", m z, "be the", group, element, "that we want the", discreteLogarithm, m b, "base", m h, "in", m grp_, "of"]
+        s ["There then exists a", m $ integer c, "such that", m c, "is the", discreteLogarithm, "base", m g, "in", m grp_, "of", m z]
+        ma $ z =: h ^ b =: g ^ c
+        let d = "d"
+        s ["Because", m h, "is an", element, "of", m grps_ <> ",", "there exists a", m $ integer d, "such that", m $ h =: g ^ d, "holds"]
+        ma $ z =: (pars $ g ^ d) ^ b =: g ^ c
+        s ["This means that we have the following equation for", m c, "in", m $ intring $ ord grps_]
+        ma $ d * b =: c
+        s ["The algorithm now uses", m a, "to find", m c, from, m z, and, m d, from, m h]
+        s ["It then computes the multiplicative inverse of", m d, "in", m $ intring $ ord grps_, "with the extended Euclidean algorithm and finally computes", m b, "by evaluating", m $ rinv d * c =: rinv d * d * b =: b]
+
+
 
 computationalDHProblemDefinition :: Note
 computationalDHProblemDefinition = de $ do
